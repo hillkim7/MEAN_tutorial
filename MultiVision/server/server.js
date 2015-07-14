a46d95c79ed820684fb515d435f7e1d84e460040
Hill Kim <flowtide@naver.com>
2015-07-14 16:43:11 +0900

    create server.js and make simple jade view page.

---
+++ MultiVision/server/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+app.get('*', function(req, res) {
+  res.render('index');
+});
+
+
+var port = 3030;
+app.listen(port);
+console.log('Listening port:', port);
+
