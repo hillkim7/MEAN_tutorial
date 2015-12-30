80fe465e22a0db4e681a8d9060f8561161125fd4
hill <hillkim@solvitsolution.co.kr>
2015-12-30 22:20:14 +0900

    Finding multiple user documents using find()

--- MeanWebDevelopment/app/controllers/users.server.controller.js
+++ MeanWebDevelopment/app/controllers/users.server.controller.js
@@ -1,5 +1,10 @@
 var User = require('mongoose').model('User');
 
+/*
+curl -X POST -H "Content-Type: application/json" -d \
+'{"firstName":"First", "lastName":"Last","email":"user@example.com","username":"username","password":"password"}' localhost:3000/users
+
+*/
 exports.create = function(req, res, next) {
   var user = new User(req.body);
 
@@ -8,6 +13,16 @@ exports.create = function(req, res, next) {
       return next(err);
     } else {
       res.json(user);
+    }
+  });
+};
+
+exports.list = function(req, res, next) {
+  User.find({}, function(err, users) {
+    if (err) {
+      return next(err);
+    } else {
+      return res.json(users);
     }
   });
 };
