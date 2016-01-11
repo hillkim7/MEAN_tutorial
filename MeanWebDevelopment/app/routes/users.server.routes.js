06072743758ba3ea9b9bcf8da6eb63c2e069cbc7
hill <hillkim7@gmail.com>
2016-01-12 01:22:42 +0900

    Add user list example which is from meanjs source.

--- MeanWebDevelopment/app/routes/users.server.routes.js
+++ MeanWebDevelopment/app/routes/users.server.routes.js
@@ -1,11 +1,11 @@
 var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app) {
-  app.route('/users')
+  app.route('/api/users')
     .post(users.create)
     .get(users.list);
     
-  app.route('/users/:userId')
+  app.route('/api/users/:userId')
     .get(users.read);
   app.param('userId', users.userByID);
 };
--- MeanWebDevelopment/public/cli_controllers/users.client.controller.js
+++ MeanWebDevelopment/public/cli_controllers/users.client.controller.js
@@ -1,6 +1,7 @@
-var mApp = angular.module('mApp', ['ui.router']);
+var usersApp = angular.module('users', ['ngResource', 'ui.router']);
+var usersAdminApp = angular.module('users.admin', ['ngResource', 'ui.router']);
 
-mApp.config(function($stateProvider, $urlRouterProvider) {
+angular.module('users').config(function ($stateProvider, $urlRouterProvider) {
     
     $urlRouterProvider.otherwise('/users');
     
@@ -18,3 +19,87 @@ mApp.config(function($stateProvider, $urlRouterProvider) {
         });
         
 });
+
+
+// Users controller
+angular.module('users').controller('UsersController', ['$scope', '$stateParams', '$location', 'Users',
+  function ($scope, $stateParams, $location, Users) {
+
+      // Create new User
+      $scope.create = function (isValid) {
+          $scope.error = null;
+
+          if (!isValid) {
+              $scope.$broadcast('show-errors-check-validity', 'userForm');
+
+              return false;
+          }
+
+          // Create new User object
+          var user = new Users({
+              title: this.title,
+              content: this.content
+          });
+
+          // Redirect after save
+          user.$save(function (response) {
+              $location.path('users/' + response._id);
+
+              // Clear form fields
+              $scope.title = '';
+              $scope.content = '';
+          }, function (errorResponse) {
+              $scope.error = errorResponse.data.message;
+          });
+      };
+
+      // Remove existing User
+      $scope.remove = function (user) {
+          if (user) {
+              user.$remove();
+
+              for (var i in $scope.users) {
+                  if ($scope.users[i] === user) {
+                      $scope.users.splice(i, 1);
+                  }
+              }
+          } else {
+              $scope.user.$remove(function () {
+                  $location.path('users');
+              });
+          }
+      };
+
+      // Update existing User
+      $scope.update = function (isValid) {
+          $scope.error = null;
+
+          if (!isValid) {
+              $scope.$broadcast('show-errors-check-validity', 'userForm');
+
+              return false;
+          }
+
+          var user = $scope.user;
+
+          user.$update(function () {
+              $location.path('users/' + user._id);
+          }, function (errorResponse) {
+              $scope.error = errorResponse.data.message;
+          });
+      };
+
+      // Find a list of Users
+      $scope.find = function () {
+          //$scope.users = Users.query();
+          $scope.rowCollection = Users.query();
+      };
+
+      // Find existing User
+      $scope.findOne = function () {
+          $scope.user = Users.get({
+              userId: $stateParams.userId
+          });
+      };
+  }
+]);
