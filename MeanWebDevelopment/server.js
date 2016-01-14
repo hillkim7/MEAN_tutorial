be1e17b1c2ed79e070b68d9dec2b413e506c39ee
hill <hillkim7@gmail.com>
2016-01-15 08:13:33 +0900

    Implementing user authentication.

--- MeanWebDevelopment/server.js
+++ MeanWebDevelopment/server.js
@@ -1,10 +1,12 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
-  express = require('./config/express');
+  express = require('./config/express'),
+  passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
+var passport = passport();
 
 var host = '0.0.0.0';
 app.listen(3000, host);
