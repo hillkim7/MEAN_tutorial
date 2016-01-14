a99799bfd0ee0237f4a135981c9c6be31dfb05bf
hill <hillkim7@gmail.com>
2016-01-14 23:40:56 +0900

    Add Grunt support.

--- MeanWebDevelopment/app/models/user.server.model.js
+++ MeanWebDevelopment/app/models/user.server.model.js
@@ -4,9 +4,26 @@
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
-  password: String
+  email: {
+    type: String,
+    // index: true,
+    match: /.+\@.+\..+/
+  },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  password: String,
+  role: {
+    type: String,
+    enum: ['Admin', 'Owner', 'User']
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('User', UserSchema);
