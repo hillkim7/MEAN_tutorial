07a449fe9044cd9309608b5a4d34b3fa8f7d44bd
hill <hillkim@solvitsolution.co.kr>
2016-01-16 14:58:43 +0900

    Example Controller is added.

--- MeanWebDevelopment/public/application.js
+++ MeanWebDevelopment/public/application.js
@@ -5,7 +5,7 @@ AngularJS manual boostrapping mechanism.
 
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
 angular.element(document).ready(function () {
   angular.bootstrap(document, [mainApplicationModuleName]);
 });
