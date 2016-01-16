0b930b2231150b8db399d448d9678940ec63e6dc
hill <hillkim@solvitsolution.co.kr>
2016-01-16 15:15:04 +0900

    angular-route is adopted.

--- MeanWebDevelopment/public/application.js
+++ MeanWebDevelopment/public/application.js
@@ -5,7 +5,18 @@ AngularJS manual boostrapping mechanism.
 
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+
+mainApplicationModule.config(['$locationProvider',
+  function ($locationProvider) {
+    // To mark your application routes as single-page application routes, you will
+    // need to use a routing scheme called Hashbangs. Hashbangs are implemented by
+    // adding an exclamation mark right after the hash sign, so an example URL would
+   // be http://localhost:3000/#!/example
+    $locationProvider.hashPrefix('!');
+  }
+]);
+
 angular.element(document).ready(function () {
   angular.bootstrap(document, [mainApplicationModuleName]);
 });
